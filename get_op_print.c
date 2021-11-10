7751e6d497b68101d4d6426394a35c078a405368
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-09 23:05:48 -0500

    fix betty

--- get_op_print.c
+++ get_op_print.c
@@ -1,7 +1,9 @@
 #include "main.h"
-/*
-* get_op_print- select the correct function
-* @format - pointer.
+
+/**
+* get_op_print - select the correct function
+* @format - pointer
+* @va_list - pointer.
 * Return:: pointer
 */
 int (*get_op_print(const char *format))(va_list)
