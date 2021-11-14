cbea341e8dda6d9637fe8de8d399d763a22e95c0
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-14 08:49:44 -0500

    fix betty

--- _printf.c
+++ _printf.c
@@ -7,37 +7,37 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
-    va_list valist;
-    int (*f)(va_list);
+	va_list valist;
+	int (*f)(va_list);
 
-    if (format == NULL)
-        return (-1);
-    va_start(valist, format);
-    while (format[i])
-    {
-        for (; format[i] != '%' && format[i]; i++)
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        if (!format[i])
-            return (count);
-        f = get_op_print(&format[i + 1]);
-        if (f != NULL)
-        {
-            count += f(valist);
-            i += 2;
-            continue;
-        }
-        if (!format[i + 1])
-            return (-1);
-        _putchar(format[i]);
-        count++;
-        if (format[i + 1] == '%')
-            i += 2;
-        else
-            i++;
-    }
-    va_end(valist);
-    return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = get_op_print(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
